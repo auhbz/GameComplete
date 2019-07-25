5904849a8ae1cd3d6def71ff89659afdf9635b00
unknown <auhbon@gmail.com>
2019-07-25 02:42:44 -0700

    first commit

---
+++ GameComplete/Assets/Scripts/Bean.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bean : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigBod;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        // float input = Input.GetAxis("Horizontal");
+        
+        rigBod.velocity = new Vector2(input*speed, rigBod.velocity.y);
+    }
+}
